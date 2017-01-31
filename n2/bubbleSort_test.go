778d88f089ae7f94c661fb530abf91052e1b7b26
Vinicius Marson <vinicius.marson@maplink.com.br>
2017-01-31 08:43:33 -0200

    Updated tests to the new go-expect interface

--- n2/bubbleSort_test.go
+++ n2/bubbleSort_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestBubbleSort(t *testing.T) {
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	theExpectedResponse := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	response := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 	BubbleSort(response)
-	expect.TheValue(response).ToBe(expected)(t)
+	expect.This(response).ToBe(theExpectedResponse)(t)
 }
